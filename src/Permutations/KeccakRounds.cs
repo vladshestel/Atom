a713bcf6d2cc73d7885799d000a2ee2dc149cb1b
Vlad Shestel <vlad.shestel@gmail>
2021-12-21 21:30:31 +0300

    issues/7 : Keccak-p Rnd function implementation

---
+++ src/Permutations/KeccakRounds.cs
@@ -0,0 +1,23 @@
+using System;
+using static Vee.Permutations;
+
+namespace Vee
+{
+    internal static class KeccakRounds
+    {
+        private static int l = 6;
+        private static int Nr = 12 + 2 * l;
+
+        internal static Span<ulong> Iterate(Span<ulong> state)
+        {
+            for (var roundIndex = 12 + 2 * l - Nr; roundIndex <= 12 + 2 * l - 1; roundIndex++) {
+                state = Round(state, roundIndex);
+            }
+
+            return state;
+        }
+
+        internal static Span<ulong> Round(Span<ulong> state, int roundIndex)
+            => Iota(Chi(Pi(Rho(Tetha(state)))), roundIndex);
+    }
+}
